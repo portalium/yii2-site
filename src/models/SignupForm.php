36f50598bcd7a7ffd48233f2db0fff5d294a0362
irmakyuceak <irmakyuceak0803@gmail.com>
2024-11-07 19:59:42 +0000

    fix:simplify user status assignment logic

--- src/models/SignupForm.php
+++ src/models/SignupForm.php
@@ -87,8 +87,7 @@ class SignupForm extends Model
         $user->access_token = \Yii::$app->security->generateRandomString();
         $user->generateAuthKey();
         $user->generateEmailVerificationToken();
-        $user->status = Yii::$app->setting->getValue('site::verifyEmail') ? User::STATUS_PASSIVE : (Yii::$app->setting->getValue('site::userStatus') == User::STATUS_ACTIVE ? User::STATUS_ACTIVE : User::STATUS_PASSIVE);
-
+        $user->status = Yii::$app->setting->getValue('site::verifyEmail') ? User::STATUS_PASSIVE : (Yii::$app->setting->getValue('site::userStatus'));
 
 
         if ($user->save()) {
